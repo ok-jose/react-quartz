3a56515e4b8f469f54cba5bd9558075bb0563cf0
Jose Lee <lg282923166@gmail.com>
2024-03-25 16:48:35 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
       fileName: 'index',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: Object.keys(pkg.peerDependencies),
       output: {
         globals: {
           react: 'React',
