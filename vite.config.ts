20a076a10c9d44b33f312ef540e597404fb9eed8
Jose Lee <lg282923166@gmail.com>
2024-03-25 15:40:53 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/lib/index.ts',
+      name: 'ReactQuartz',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
