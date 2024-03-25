0bcfb0ef8c544ea75cc0fe83ab8b0960d5640384
Jose Lee <lg282923166@gmail.com>
2024-03-25 15:40:53 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'ReactQuartz',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: Object.keys(pkg.peerDependencies),
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
