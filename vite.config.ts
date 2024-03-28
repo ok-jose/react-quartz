4e4a149084cb0c6bc0025d11026cea3e53473c01
Jose <lg282923166@gmail.com>
2024-03-28 14:39:03 +0800

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dtsPlugin from 'vite-plugin-dts'
 import path from 'path'
 import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dtsPlugin({ rollupTypes: true }), react()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
